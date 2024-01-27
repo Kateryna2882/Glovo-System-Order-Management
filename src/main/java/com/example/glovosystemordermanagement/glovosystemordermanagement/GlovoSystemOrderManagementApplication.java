aa09111711a983556ea4d5d6708cbcc7d9538989
Kateryna2882 <govoryhina82@gmail.com>
2024-01-27 16:50:01 +0200

    Initial commit

---
+++ src/main/java/com/example/glovosystemordermanagement/glovosystemordermanagement/GlovoSystemOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.glovosystemordermanagement.glovosystemordermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlovoSystemOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlovoSystemOrderManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/glovosystemordermanagement/glovosystemordermanagement/GlovoSystemOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glovosystemordermanagement.glovosystemordermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlovoSystemOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
